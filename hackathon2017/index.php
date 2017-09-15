39a48dc2dde672db0758d3c963748879ef1a22f9
Chloie Marba <hanchloie22@gmail.com>
2017-09-16 06:42:43 +0800

    Navbar stuff and icon

--- hackathon2017/index.php
+++ hackathon2017/index.php
@@ -2,12 +2,14 @@
 <html ng-app="myApp">
 <head>
 	<title>Pagta-o</title>
+	<link rel="icon" class="glyphicon glyphicon-link" />
 
 	<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet"> -->
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
 	<link href="css/bootswatchpapertheme.min.css" rel="stylesheet">
 	<link href="css/default.css" rel="stylesheet">
 	<link href="css/login.css" rel="stylesheet">
+	<link href="css/home.css" rel="stylesheet">
 
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>-->
