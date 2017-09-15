e47121c1b6bd73e41358ba3c0c731f98c6e7415d
unknown <nuj101nuj@gmail.com>
2017-09-15 23:17:46 +0800

    layout for login page

--- hackathon2017/index.php
+++ hackathon2017/index.php
@@ -6,6 +6,7 @@
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootswatchpapertheme.min.css" rel="stylesheet">
 	<link href="css/default.css" rel="stylesheet">
+	<link href="css/login.css" rel="stylesheet">
 
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
