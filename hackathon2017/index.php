f1307eb2f1f42eee54789e64b0160412797baefc
unknown <nuj101nuj@gmail.com>
2017-09-16 08:00:02 +0800

    Pulling changes

--- hackathon2017/index.php
+++ hackathon2017/index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html ng-app="myApp">
 <head>
-	<title>Pagbulig - Hackathon 2017</title>
+	<title>Pagta-o</title>
+	<link rel="icon" class="glyphicon glyphicon-link" />
 
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet"> -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
 	<link href="css/bootswatchpapertheme.min.css" rel="stylesheet">
 	<link href="css/default.css" rel="stylesheet">
 	<link href="css/login.css" rel="stylesheet">
+	<link href="css/home.css" rel="stylesheet">
 
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>-->
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.6/angular-route.js"></script>
 	<script src="controllers/indexController.js"></script>
@@ -17,8 +20,18 @@
 	<script src="app.js"></script>
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="navbar-brand"><a href="#!/">Pagbulig</a></div>
+	<nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#!/"><span class="glyphicon glyphicon-link"></span> Pagta-o</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#!/login">Login</a></li>
+					<li><a href="#!/register">Register</a></li>
+		        </ul>
+		    </div>
+		</div>
 	</nav>
 	<section ng-view></section>
 	<footer>
