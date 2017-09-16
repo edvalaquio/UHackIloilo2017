545b11fbae47225e46665e25aaf4a2601fdd2c3b
Chloie Marba <hanchloie22@gmail.com>
2017-09-16 08:43:30 +0800

    home page edits

--- hackathon2017/partials/home.php
+++ hackathon2017/partials/home.php
@@ -1,21 +1,41 @@
-<div class="container cont-1">
-	<div class="row bg-1">
-		<div class="col-lg-12">
-			<h2 class="text-center">Pagta-o</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+<div class="container">
+	<header>
+		<div class="jumbotron jumbotron-fluid" id="banner">
+			<div class="parallax text-center bg-color">
+				<div class="parallax-pattern-overlay">
+					<div class="container text-center heightpadding">
+						<!-- <a href="#"></a> site logo sana-->
+						<h2 class="display-2">Change starts with you.</h2>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-	<div class="row">
-		<div class="col-lg-12">
-			<h2>Features</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+	</header>
+	<section class="about-us" id="about-us">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-12">
-			<h2>Contact us!</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+	</section>
+	<section class="features" id="features">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+				</div>
+			</div>
 		</div>
-	</div>
+	</section>
+	<section class="contact-us" id="contact-us">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
