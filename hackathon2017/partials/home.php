25ba53bb6239fdd1d6c98d27707a9b374339b275
Chloie Marba <hanchloie22@gmail.com>
2017-09-16 09:00:46 +0800

    home page edits

--- hackathon2017/partials/home.php
+++ hackathon2017/partials/home.php
@@ -1,10 +1,10 @@
-<div class="container">
+<div class="wrapper">
 	<header>
 		<div class="jumbotron jumbotron-fluid" id="banner">
 			<div class="parallax text-center bg-color">
 				<div class="parallax-pattern-overlay">
 					<div class="container text-center heightpadding">
-						<!-- <a href="#"></a> site logo sana-->
+						 <a href="#"><img id="site-title" src="img/nav-icon.png" alt="logo" /></a>
 						<h2 class="display-2">Change starts with you.</h2>
 					</div>
 				</div>
@@ -15,7 +15,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+					<p><strong>Pagta-o</strong>, a Kinaray-a word which translates to "giving", is an online platform whose goal is to help individuals and organizations initiate outreach programs in places that have been affected by natural disasters, corrupted by pollution, or torn by the destruction of war. </p>
 				</div>
 			</div>
 		</div>
