6d8686fec0de3abca17ddecd240d44017d866d02
Chloie Marba <hanchloie22@gmail.com>
2017-09-16 09:29:23 +0800

    home page edits

--- hackathon2017/partials/home.php
+++ hackathon2017/partials/home.php
@@ -11,31 +11,51 @@
 			</div>
 		</div>
 	</header>
-	<section class="about-us" id="about-us">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<p><strong>Pagta-o</strong>, a Kinaray-a word which translates to "giving", is an online platform whose goal is to help individuals and organizations initiate outreach programs in places that have been affected by natural disasters, corrupted by pollution, or torn by the destruction of war. </p>
-				</div>
+	<div class="jumbotron jumbotron-fluid" id="banner">
+		<div class="parallax text-center">
+			<div class="parallax-pattern-overlay">
+				<section class="about-us" id="about-us">
+					<div class="container text-center heightpadding">
+						<div class="row">
+							<div class="col-lg-12">
+								<p class="display-3"><strong>Pagta-o</strong>, a Kinaray-a word which translates to "giving", is an online platform whose goal is to help individuals and organizations initiate outreach programs in places that have been affected by natural disasters, corrupted by pollution, or torn by the destruction of war. </p>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		</div>
-	</section>
-	<section class="features" id="features">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
-				</div>
+	</div>
+	<div class="jumbotron jumbotron-fluid" id="banner">
+		<div class="parallax text-center bg-color">
+			<div class="parallax-pattern-overlay">
+				<section class="features" id="features">
+					<div class="container heightpadding">
+						<div class="row">
+							<div class="col-lg-12">
+								<p class="display-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		</div>
-	</section>
-	<section class="contact-us" id="contact-us">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac leo quam. Aenean gravida diam vitae luctus bibendum. Ut dignissim laoreet velit at tempor. Donec pulvinar eros at enim molestie, volutpat aliquet ligula ultrices. Quisque pharetra suscipit felis, vitae commodo felis sagittis nec. Aenean accumsan metus vel suscipit tincidunt. Sed eget nisi at urna pretium pretium. Morbi cursus semper quam, id tempus lacus finibus ac. Nulla non elit risus. Donec ac feugiat ex, eu molestie lorem. In imperdiet nulla eros, nec ultricies magna posuere et. Duis lacus metus, aliquet vel ullamcorper ac, condimentum nec risus. Aliquam quis interdum libero, in sagittis ex. Proin iaculis turpis et mauris cursus bibendum nec eget est. Duis malesuada suscipit ex at pretium.</p>
-				</div>
+	</div>
+	<div class="jumbotron jumbotron-fluid" id="banner">
+		<div class="parallax text-center">
+			<div class="parallax-pattern-overlay">
+				<section class="join-us" id="join-us">
+					<div class="container text-center heightpadding">
+						<div class="row">
+							<div class="col-lg-12">
+								<h2 class="display-1"><i>"Updi kame sa pagta-o."</i></h2>
+								<h3 class="display-3"><i>"Join us in giving."</i></h3>
+								<button type="submit" class="btn btn-primary">Serve the people</button>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		</div>
-	</section>
+	</div>
 </div>
